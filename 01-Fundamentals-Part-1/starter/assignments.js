005e7fb3e4b0f3c253660f5d32015b2d04a66cbe
Michael <mikedobbs1997@gmail.com>
2024-02-15 17:31:03 -0600

    Completed variables assignment. Linked script to lesson index.html and verified that the solution was working without issue.

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,6 @@
+let country = 'America';
+let continent = 'North America';
+let population = 340000000;
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population: " + population);
