25cda5713247f97009f9ab925dd42ab59e6b54bb
Michael <mikedobbs1997@gmail.com>
2024-02-15 21:34:49 -0600

    Updated course scripts and notes with template literal information.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,31 +1,44 @@
+//Display
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
-
 console.log('Jonas');
 console.log(23);
 
+//variables
 let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 console.log(typeof true);
 console.log(typeof javascriptIsFun);
 console.log(typeof 23);
 console.log(typeof 'Jonas');
-
 const PI = 3.1415;
 var test = 'test';
 
+//operators
 const now = 2024;
 const ageMike = 2024 - 1997;
 const ageHarper = 2024 - 2001;
 console.log(ageMike, ageHarper);
 console.log(ageMike * 2, ageHarper / 10, 2 ** 3);
-
 let x = 2;
 let y = 4;
 y += 1;
 console.log(x <= y);
+
+//strings
+const fName = "Michael";
+const job = 'IT Specialist';
+const year = '2024';
+let message = "I'm " + fName + ", a " + (year - 1997) + " year old " + job;
+console.log(message);
+
+//template literals using ``
+const age = year - 1997
+const messageLiteral = `I'm ${fName}, a ${age} year old ${job}.`;
+console.log(messageLiteral);
+
+//decisions
