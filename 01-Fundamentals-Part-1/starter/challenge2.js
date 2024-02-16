c5d29a7016e98dee9300a225aced90689d672ff6
Michael <mikedobbs1997@gmail.com>
2024-02-16 08:26:54 -0600

    Completed coding challege #2 and uploaded code.

--- 01-Fundamentals-Part-1/starter/challenge2.js
+++ 01-Fundamentals-Part-1/starter/challenge2.js
@@ -22,3 +22,9 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 console.log(BMIMark, BMIJohn);
 
 /* Write your code below. Good luck! 🙂 */
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+}
+else {
+    console.log(`John's BMI ${BMIJohn} is higher than Marks's ${BMIMark}!`);
+}
