4d853f3ae7063409eb696fdb35ef7d0ba3b33fff
Michael <mikedobbs1997@gmail.com>
2024-02-16 20:05:44 -0600

    Added notes for strict mode and some scripts.

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* The following examples show how strict mode catches extra errors.
+let hasDriversLicense = false;
+const passTest = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive.`);
+const interface = 'Test'
+const private = 'Test'
+*/
+
+//Functions
