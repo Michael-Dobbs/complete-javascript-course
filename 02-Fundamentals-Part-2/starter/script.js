f87df2c940fbcf428048a4bd8a03133648f216f6
Michael <mikedobbs1997@gmail.com>
2024-02-18 20:45:40 -0600

    Added more object notes and scripts. Completed coding challenege #5

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -94,3 +94,79 @@ friends.shift();
 friends.indexOf('Tom');
 //Includes - Returns true/false
 friends.includes('Tom');
+
+//Objects
+const michael = {
+    firstName: 'Michael',
+    lastName: 'Dobbs',
+    age: '27',
+    job: 'IT Specialist',
+    friends: ['Logan', 'Rick', 'Connor']
+}
+console.log(michael)
+
+//Retrieving object 
+console.log(michael.lastName);
+console.log(michael['lastName']); //This version allows for expressions to be entered. Example below.
+
+//We can retrieve firstName and lastName with expressions.
+const nameKey = 'Name';
+console.log(michael['first' + nameKey]);
+console.log(michael['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Michael? (Choose firstName, lastName, age, job, and friends)')
+
+if (michael[interestedIn]) {
+    console.log(michael[interestedIn]);
+} else {
+    console.log(`Wrong Request! (Choose firstName, lastName, age, job, and friends)`);
+}
+
+michael.location = 'America';
+michael['website'] = 'meetmichaeldobbs.com';
+console.log(michael);
+
+//Challenge! Write the following. Michael has 3 friends. His best friend is called Logan.
+
+console.log(`${michael.firstName} has ${michael.friends.length} friends. His best friend is called ${michael.friends[0]}.`);
+
+//Object Methods
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'Teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    calcAge: function (birthYear) {
+        return 2037 - birthYear;
+    },
+
+    calcAge2: function () {
+        console.log(this)
+        return 2037 - this.birthYear
+    },
+
+    calcAndStore: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge2()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`;
+    }
+}
+
+//Calling the object methods
+console.log(jonas.calcAge(1991));
+console.log(jonas['calcAge'](1991));
+
+//Using 'this' keyword.
+console.log(jonas.calcAge2());
+console.log(jonas.calcAndStore());
+
+
+//Challenge! Write the following:
+//"Jonas is a 46-year old teacher, and he has a drivers license.""
+console.log(jonas.getSummary());
