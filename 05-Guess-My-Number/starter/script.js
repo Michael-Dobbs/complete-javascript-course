d53af7f3b6a30365b01c190b1a7a7f293a2f74a2
Michael <mikedobbs1997@gmail.com>
2024-03-11 16:54:03 -0500

    Added DOM Notes and script start.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+/*document.querySelector() allows us to select and interact with elements, child elements, and their text/values.*/
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// //With input fields we use the value property.
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+
+//
+document.querySelector('.check').addEventListener('click');
